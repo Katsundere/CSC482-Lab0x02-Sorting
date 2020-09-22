1d72fb9311221b7c6a58cce87fe93cd5f9639ff3
Jensen <ty.jensen@trojans.dsu.edu>
2020-09-22 17:47:22 -0600

    Added the time function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
-
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 public class Main {
 
     public static void main(String[] args) {
@@ -7,5 +8,13 @@ public class Main {
         int N; //The length of list to be sorted
         int k; // The size of the key-values(strings) to sort
         int d; // The size (in bytes or characters) of the digits used by the radix sort.
+    }
+    /** Get CPU time in nanoseconds since the program(thread) started. */
+    /** from: http://nadeausoftware.com/articles/2008/03/java_tip_how_get_cpu_and_user_time_benchmarking#TimingasinglethreadedtaskusingCPUsystemandusertime **/
+    public static long getCpuTime( ) {
+        ThreadMXBean bean = ManagementFactory.getThreadMXBean( );
+        return bean.isCurrentThreadCpuTimeSupported( ) ?
+                bean.getCurrentThreadCpuTime( ) : 0L;
+
     }
 }
