7acbe9dcea47a11952f7c63019ed964221cd14d4
Jensen <ty.jensen@trojans.dsu.edu>
2020-09-23 01:35:02 -0600

    Finished the merge sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,16 @@ import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadMXBean;
 import java.util.Random;
 
+
 public class Main {
-    private static int N = 100;          //The length of list to be sorted
+    private static int N = 3;          //The length of list to be sorted
     private static int k = 4;           // The size of the key-values(strings) to sort
     private static int d = 0;           // The size (in bytes or characters) of the digits used by the radix sort
     private static int minV = 97;       // smallest value for char
     private static int maxV = 122;      // Largest value for char
 
     //Function to generate the test list
-    public static String[] generateTestList(int N, int k, int minV, int maxV){
+    public static String[] generateTestList(){
         String[] generateList = new String[N];                                         // Makes a new string Array of size N
         char[] generateChar = new char[k+1];                                           // Makes a char array of size k+1
         Random rnd = new Random();
@@ -24,7 +25,7 @@ public class Main {
             generateChar[k] = '\0';                                                     // appends a null terminator to generateChar at position k in the array. (The end of the segment)
             generateList[i] = String.valueOf(generateChar);                             //puts the character segments into the string array at position i
         }
-        return generateList;
+        return generateList;                                                            // returns the generated list to main function
     }
     public static void print(String data[]){                                            // print function with some formatting.
         System.out.print("{ ");
@@ -33,7 +34,7 @@ public class Main {
         }
         System.out.println("\b\b\b }");                                                 // \b used to erase characters at the end I don't want printed out.
     }
-    public static String[] insertionSort(String data[], int N){                         // start of insertion sort.
+    public static String[] insertionSort(String data[]){                                // start of insertion sort.
         String temp;
 
         for(int i = 0; i < N; i++){                                                     //loops run until size N is reached
@@ -45,15 +46,50 @@ public class Main {
                 }
             }
         }
-        return data;
+        return data;                                                                    // returns the sorted array to the main function
+    }
+    public static String[] mergeSort(String data[], int low, int high){                                    //  Start of merge sort
+        int middle = (int) Math.ceil((high - low)/2) + low;
+        String[] sort = new String[high-low];
+        if(high - low >= 2) {
+            String[] left = mergeSort(data, low, middle);
+            String[] right = mergeSort(data, middle, high);
+            int l = 0;
+            int h = 0;
+            for (int i = 0; i < high - low; i++) {
+                if (l >= middle - low && h < high - middle) {
+                    sort[i] = right[h];
+                    h++;
+                } else if (h >= high - middle && l < middle - low) {
+                    sort[i] = left[l];
+                    l++;
+                } else if (left[l].compareToIgnoreCase(right[h]) < 0) {
+                    sort[i] = left[l];
+                    l++;
+                } else {
+                    sort[i] = right[h];
+                    h++;
+                }
+            }
+        }
+        else {
+            sort[0] = data[low];
+        }
+        return sort;
     }
 
     public static void main(String[] args) {                                            // Start of main function
         String[] testList = new String[N];
         String[] sortedList = new String[N];
-        testList = generateTestList(N, k, minV, maxV);
+        System.out.println("Insertion sort");
+        testList = generateTestList();
         print(testList);
-        sortedList = insertionSort(testList, N);
+        sortedList = insertionSort(testList);
+        print(sortedList);
+        System.out.println("Merge Sort");
+        testList = generateTestList();
+        print(testList);
+        sortedList = mergeSort(testList, 0, N);
         print(sortedList);
 
     }
